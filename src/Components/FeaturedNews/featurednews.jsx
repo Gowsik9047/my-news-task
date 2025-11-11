e772a3b6624d0e053fa9c321ca14ca0d4f34d619
Gowsik9047 <gowsik9047@gmail.com>
2025-11-11 23:07:36 +0530

    bug check commit

--- src/Components/FeaturedNews/featurednews.jsx
+++ src/Components/FeaturedNews/featurednews.jsx
@@ -6,9 +6,9 @@ import NewsCard from "../NewsCard/newscard";
 import featuredData from "../../Data/datafeatured";
 
 export default function FeaturedNews() {
-  const [hero, ...rest] = featuredData;
-  const side = rest.slice(0, 2);
-  const smalls = rest.slice(2);
+  // const [hero, ...rest] = featuredData;
+  // const side = rest.slice(0, 2);
+  // const smalls = rest.slice(2);
 
   return (
     <section className="featured-wrapper">
@@ -18,18 +18,18 @@ export default function FeaturedNews() {
         </div>
 
         <div className="side-col">
-          {side.map((s) => (
+          {/* {side.map((s) => ( */}
             <></>
-            // <NewsCard key={s.id} article={s} variant="side" />
-          ))}
+            {/* // <NewsCard key={s.id} article={s} variant="side" /> */}
+          {/* ))} */}
         </div>
       </div>
 
       <div className="small-grid">
-        {smalls.map((s) => (
+        {/* {smalls.map((s) => ( */}
           <></>
-          // <NewsCard key={s.id} article={s} variant="small" />
-        ))}
+          {/* // <NewsCard key={s.id} article={s} variant="small" /> */}
+        {/* ))} */}
       </div>
     </section>
   );
