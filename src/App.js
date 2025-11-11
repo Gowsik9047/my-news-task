749c6fffd190197f0d7fd566adb1d3fe986f4633
Gowsik9047 <gowsik9047@gmail.com>
2025-11-11 22:16:32 +0530

    changes commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to My React App</h1>
-      <p>This is a simple React application.</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
