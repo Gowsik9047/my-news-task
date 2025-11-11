7d8004f79af4812fe6cbcd3a5d8ccf9be7fa11b3
Gowsik9047 <gowsik9047@gmail.com>
2025-11-11 22:57:07 +0530

    bug check commit

--- src/Components/LiveTicker/liveticker.jsx
+++ src/Components/LiveTicker/liveticker.jsx
@@ -4,9 +4,7 @@ function LiveTicker() {
   return (
     <div className="live-header">
       <button className="live">LIVE</button>
-      {/* <marquee behavior="scroll" direction="left">
-        உடனடி: மைல்கல் சிவில் உரிமைகள் வழக்கில் நீதிமன்ற தீர்ப்பு
-      </marquee> */}
+      <h4>உடனடி: மைல்கல் சிவில் உரிமைகள் வழக்கில் நீதிமன்ற தீர்ப்பு</h4>
       <i className="fa-solid fa-close"></i>
     </div>
   );
