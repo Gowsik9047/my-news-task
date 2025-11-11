4b4eee19e32aed4a5c5ba57e4225a3e1e206366b
Gowsik9047 <gowsik9047@gmail.com>
2025-11-11 22:41:13 +0530

    other component impl

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,17 +1,17 @@
 import Navbar from "../Components/Navbar/navbar";
 // import LiveTicker from "../Components/LiveTicker/liveticker";
-// import AdBanner from "../Components/AdBanner/adbanner";
-// import FeaturedNews from "../Components/FeaturedNews/featurednews";
-// import NewsGrid from "../Components/NewsGrid/newsgrid";
+import AdBanner from "../Components/AdBanner/adbanner";
+import FeaturedNews from "../Components/FeaturedNews/featurednews";
+import NewsGrid from "../Components/NewsGrid/newsgrid";
 
 function Home() {
   return (
     <>
       <Navbar />
       {/* <LiveTicker /> */}
-      {/* <FeaturedNews /> */}
-      {/* <AdBanner /> */}
-      {/* <NewsGrid /> */}
+      <FeaturedNews />
+      <AdBanner />
+      <NewsGrid />
     </>
   );
 }
