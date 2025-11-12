c676041b16c2e27e4711b74e76758c8406994788
Gowsik9047 <gowsik9047@gmail.com>
2025-11-12 19:57:49 +0530

    bug check commit

--- src/Components/FeaturedNews/featurednews.jsx
+++ src/Components/FeaturedNews/featurednews.jsx
@@ -1,14 +1,5 @@
-import img1 from "../../assets/images/heading image 1.jpg"
-import img2 from "../../assets/images/heading image 2.jpg"
-import img3 from "../../assets/images/heading image 3.jpg"
-import img4 from "../../assets/images/img1.jpg"
-import img5 from "../../assets/images/img2.jpg"
-import img6 from "../../assets/images/img3.jpg"
-import img7 from "../../assets/images/img4.jpg"
-
-import NewsCard from "../NewsCard/newscard";
+// import NewsCard from "../NewsCard/newscard";
 import featuredData from "../../Data/datafeatured";
-import React from "react";
 
 function FeaturedNews() {
   const [hero, ...rest] = featuredData;
